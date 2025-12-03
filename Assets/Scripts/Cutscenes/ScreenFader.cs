628380572a26c36df56e84394b146eae1b2824a3
Leafionx <chayakhon25@gmail.com>
2025-12-04 03:31:33 +0700

    Red

---
+++ Assets/Scripts/Cutscenes/ScreenFader.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScreenFader : MonoBehaviour
+{
+    public static ScreenFader Instance;
+
+    public Image fadeImage;
+    public float fadeDuration = 0.6f;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        fadeImage.gameObject.SetActive(false);
+    }
+
+    public IEnumerator FadeOut()
+    {
+        fadeImage.gameObject.SetActive(true);
+        fadeImage.raycastTarget = true;
+
+        float t = 0;
+        while (t < fadeDuration)
+        {
+            t += Time.unscaledDeltaTime;
+            fadeImage.color = new Color(0, 0, 0, Mathf.Lerp(0, 1, t / fadeDuration));
+            yield return null;
+        }
+    }
+
+    public IEnumerator FadeIn()
+    {
+        float t = 0;
+        while (t < fadeDuration)
+        {
+            t += Time.unscaledDeltaTime;
+            fadeImage.color = new Color(0, 0, 0, Mathf.Lerp(1, 0, t / fadeDuration));
+            yield return null;
+        }
+
+        fadeImage.gameObject.SetActive(false);
+        fadeImage.raycastTarget = false;
+    }
+}

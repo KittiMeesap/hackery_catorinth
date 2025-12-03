0a1f1d096788d0136bb315d28c2bea026499d87f
Kitti Meesap <tkt.ms@hotmail.com>
2025-12-04 04:24:32 +0700

    Fix Yellow

--- Assets/Scripts/Core/UIManager.cs
+++ Assets/Scripts/Core/UIManager.cs
@@ -27,7 +27,7 @@ public class UIManager : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);
+            //DontDestroyOnLoad(gameObject);
         }
         else
         {
